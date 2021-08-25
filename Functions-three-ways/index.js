cad4b88bc8c620e94be4d0ace16e5c7793e9731b
Jayh01 <84378798+Jayh01@users.noreply.github.com>
2021-08-25 12:29:48 +0200

    Add files via upload

---
+++ Functions-three-ways/index.js
@@ -0,0 +1,25 @@
+function voorbeeld1 (som1,som2) {
+    optellen = som1 * som2;
+        totaal = optellen * optellen
+        console.log (totaal);
+}   
+voorbeeld1 (2,2);
+
+
+const voorbeeld2 = function (som1,som2) {
+    optellen = som1 * som2 ;
+        console.log (optellen);
+}
+voorbeeld2 (2,2);
+
+
+const voorbeeld3 = (som1,som2) => {
+    optellen = som1 * som2 ;
+        console.log (optellen);
+}
+voorbeeld3 (2,2);
+
+
+
+
+
